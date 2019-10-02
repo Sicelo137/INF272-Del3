98b3f12afe8aad5c0161bc5d78e9f60ad36638d4
Sicelo137 <55740755+Sicelo137@users.noreply.github.com>
2019-10-02 13:52:15 +0200

    Deliverable 3

---
+++ INF272_Deliverable_2/INF272_Deliverable_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="INF272_Deliverable_2.MvcApplication" Language="C#" %>
---
+++ INF272_Deliverable_2/INF272_Deliverable_2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(INF272_Deliverable_2.Startup))]
+namespace INF272_Deliverable_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
